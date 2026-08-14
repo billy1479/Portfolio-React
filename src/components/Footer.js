5f7c4ee36ddd5313ed11e5cc50a1851b8b3df243
Billy Stapleton <59696174+billy1479@users.noreply.github.com>
2026-08-14 13:21:29 +0100

    Update to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,6 @@ const Footer = () => {
   return (
     <footer className="w-full min-w-0 mx-auto text-center py-6 text-gray-500 border-t border-gray-200">
       <p>© {new Date().getFullYear()} William Stapleton. All rights reserved.</p>
-      <p className="mt-1 text-sm">Made with React & Tailwind CSS</p>
     </footer>
   );
 };
